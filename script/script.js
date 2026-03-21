a785ef9315e4fb84f874ca0db508b2be4f1a6ae1
jubayer50 <aujubayer1@gmail.com>
2026-03-21 16:06:06 +0600

    all trees loaded from API and show display

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const categoriesBtnsContainer = document.getElementById(
   "category-btn-container",
 );
+const allTreesContainer = document.getElementById("all-trees-contaienr");
 
 async function loadCategory() {
   const url = "https://openapi.programming-hero.com/api/categories";
@@ -24,3 +25,48 @@ async function loadCategory() {
 }
 
 loadCategory();
+
+async function loadAllTrees() {
+  const url = "https://openapi.programming-hero.com/api/plants";
+
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data);
+  allTreesDisplay(data.plants);
+}
+
+loadAllTrees();
+
+function allTreesDisplay(trees) {
+  trees.forEach((tree) => {
+    // console.log(tree);
+    const div = document.createElement("div");
+    div.className = "bg-white rounded-lg p-2";
+    div.innerHTML = `
+                <div>
+                  <img class="h-40 w-full object-cover rounded-md" src=${tree.image} alt="" />
+                </div>
+                <div class="mt-3 space-y-3">
+                  <h4 class="font-semibold text-[14px] text-[#1F2937]">
+                   ${tree.name}
+                  </h4>
+                  <p class="text-[12px] text-[#1F2937] line-clamp-2">
+                    ${tree.description}
+                  </p>
+                  <div class="flex justify-between items-center">
+                    <p
+                      class="text-[#15803D] font-bold text-[12px] bg-[#DCFCE7] py-1 px-2 rounded-full"
+                    >
+                      ${tree.category}
+                    </p>
+                    <p class="text-[14px] font-semibold">${tree.price}</p>
+                  </div>
+                  <button
+                    class="btn bg-[#15803D] rounded-full font-bold text-white w-full"
+                  >
+                    Add to Cart
+                  </button>
+    `;
+    allTreesContainer.append(div);
+  });
+}
