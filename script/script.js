bf01caf7b188c60cbfd4cc6c8409c02b44a619ac
jubayer50 <aujubayer1@gmail.com>
2026-03-21 13:31:43 +0600

    category ways btn load from API

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log("connect");
+const categoriesBtnsContainer = document.getElementById(
+  "category-btn-container",
+);
+
+async function loadCategory() {
+  const url = "https://openapi.programming-hero.com/api/categories";
+
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data);
+
+  const btnCategorys = data.categories;
+
+  // --------------------------------
+
+  btnCategorys.forEach((category) => {
+    // console.log(category);
+    const btn = document.createElement("button");
+    btn.className =
+      "btn border-1 bg-transparent border-[#15803D] text-[#15803D] w-full";
+    btn.innerText = category.category_name;
+    categoriesBtnsContainer.append(btn);
+  });
+}
+
+loadCategory();
