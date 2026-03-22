a2611fefd6522a063ddd7a76bae07a62796fcca5
jubayer50 <aujubayer1@gmail.com>
2026-03-22 21:14:12 +0600

    cart function implemented

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ const categoriesBtnsContainer = document.getElementById(
 const allTreesContainer = document.getElementById("all-trees-contaienr");
 const loadingContainer = document.getElementById("loading-container");
 const plantDetailModal = document.getElementById("plant_details_modal");
+const cartContent = document.getElementById("cart-content");
+const totalPrice = document.getElementById("total-price");
+const emptyCartMassage = document.getElementById("empty-cart-massage");
+let cart = [];
 
 // modal
 const modalTitle = document.getElementById("modal-title");
@@ -27,7 +31,7 @@ async function loadCategory() {
     // console.log(category);
     const btn = document.createElement("button");
     btn.className =
-      "rounded-md py-1.5 border-1 border-[#15803D] text-[#15803D] w-full";
+      "rounded-md cursor-pointer py-1.5 border-1 border-[#15803D] text-[#15803D] w-full";
     btn.innerText = category.category_name;
     btn.onclick = () => selectCategoryBtn(category.id, btn);
     categoriesBtnsContainer.append(btn);
@@ -64,13 +68,13 @@ function allTreesDisplay(trees) {
   trees.forEach((tree) => {
     // console.log(tree);
     const div = document.createElement("div");
-    div.className = "bg-white rounded-lg p-2";
+    div.className = "bg-white rounded-lg p-2 shadow";
     div.innerHTML = `
                 <div>
-                  <img class="h-[180px] w-full object-cover rounded-md" src=${tree.image} alt="" />
+                  <img class="h-[180px] w-full object-cover hover:scale-103 transition-transform duration-500 rounded-md" src=${tree.image} alt="" />
                 </div>
                 <div class="mt-3 space-y-3">
-                  <h4 class="font-semibold text-[14px] text-[#1F2937] hover:text-[#15803D]" onclick='openModal(${tree.id})'>
+                  <h4 class="font-semibold text-[14px] text-[#1F2937] cursor-pointer hover:text-[#15803D]" onclick='openModal(${tree.id})'>
                    ${tree.name}
                   </h4>
                   <p class="text-[12px] text-[#1F2937] line-clamp-2">
@@ -85,6 +89,7 @@ function allTreesDisplay(trees) {
                     <p class="text-[14px] font-semibold">${tree.price}</p>
                   </div>
                   <button
+                  onclick="addToCart(${tree.id},'${tree.name}',${tree.price})"
                     class="btn bg-[#15803D] rounded-full font-bold text-white w-full"
                   >
                     Add to Cart
@@ -111,8 +116,7 @@ async function selectCategoryBtn(categoryId, btn) {
 
   // console.log(allCategoriesBtns);
 
-  const url = `https://openapi.programming-hero.com/api/category/${categoryId}
-`;
+  const url = `https://openapi.programming-hero.com/api/category/${categoryId}`;
 
   const res = await fetch(url);
   const data = await res.json();
@@ -154,4 +158,64 @@ async function openModal(treeId) {
   modalDescription.innerText = treeDetails.description;
 
   plantDetailModal.showModal();
+}
+
+function addToCart(id, name, price) {
+  // console.log(id, name, price, "add to card");
+
+  let objectTree = {
+    id,
+    name,
+    price,
+    quantity: 1,
+  };
+
+  const treeExisting = cart.find((item) => item.id === id);
+
+  if (treeExisting) {
+    treeExisting.quantity++;
+  } else {
+    cart.push(objectTree);
+  }
+
+  updateCart();
+  // console.log(cart);
+}
+
+function updateCart() {
+  cartContent.innerHTML = "";
+
+  if (cart.length === 0) {
+    emptyCartMassage.classList.remove("hidden");
+  } else {
+    emptyCartMassage.classList.add("hidden");
+  }
+
+  let totalAmount = 0;
+
+  cart.forEach((item) => {
+    totalAmount = totalAmount + item.price * item.quantity;
+
+    const cartDiv = document.createElement("div");
+    cartDiv.className = "bg-[#F0FDF4] p-4 rounded-xl";
+    cartDiv.innerHTML = `
+              <div class="flex justify-between items-center">
+                <h3 class="font-bold text-[#1F2937]">${item.name}</h3>
+                <button onclick='cartRemove(${item.id})' class="btn btn-ghost btn-circle">X</button>
+              </div>
+              <p class="text-[#8C8C8C]">${item.price} x ${item.quantity}</p>
+              <p class="text-right font-medium">${item.price * item.quantity}</p>
+    `;
+    cartContent.append(cartDiv);
+  });
+
+  totalPrice.innerText = totalAmount;
+}
+
+// cart remove function
+function cartRemove(id) {
+  let newCart = cart.filter((item) => item.id != id);
+
+  cart = newCart;
+  updateCart();
 }
